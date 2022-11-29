7882ffe1298e10e050a4845931d1cafb62184295
Jubayer Hossain <93054201+Lycanthrope8@users.noreply.github.com>
2022-11-30 00:10:30 +0600

    Add files via upload

--- project/home.php
+++ project/home.php
@@ -1,31 +1,101 @@
+
 <?php
+
 include('config.php');
 include('login.php');
 
 $username=$_SESSION['username'];
-echo "Welcome ", $username;
-
+$user_pexpenses=$username.'_Pexpenses';
 ?>
 
 <!DOCTYPE html>
 <html>
+    <head>
+
+    </head>
+
     <body>
-        <H1>Connect Hoya gese!!!</H1>
-
-        <h1>Personal Expenses</h1>
-        <table>
-            <tr>
-                <th>Date</th>
-                <th>Time</th>
-                <th>Spended On</th>
-                <th>Amount</th>
-            </tr>
-        </table>
-
+        <!-- <?php require_once 'process.php'; ?> -->
+        <!-- Session message. Div is for design purpose -->
+        <?php if(isset($_SESSION['message'])):?>
+        <div> 
+            <?php echo $_SESSION['message']; 
+                unset($_SESSION['message']);
+            ?>
+        </div>
+        <?php endif; ?>
+        <!-- Query to show whole data table -->
         <?php
-        // $sql="SELECT DS,TS,descr,ammount FROM $User_Pexpenses";
-        // $result= $con-> query(sql);
-        // echo "HELLO"
+        $result = $con->query("SELECT * FROM $user_pexpenses ORDER BY PexpenseID DESC");
+            ?>
+            <div>
+                <!--Creating data table-->
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Description</th>
+                            <th>Amount</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th colspan='2'>Action</th>
+                        </tr>
+                    </thead>
+                    <!--Loop to see the fetched data table-->
+
+                    <?php
+                        while ($row = $result->fetch_assoc()):
+                    ?>
+
+                        <tr>
+                            <td> <?php echo $row['PexpenseID']; ?> </td>
+                            <td> <?php echo $row['descr']; ?> </td>
+                            <td> <?php echo $row['amount']; ?> </td>
+                            <td> <?php echo $row['DS']; ?> </td>
+                            <td> <?php echo $row['TS']; ?> </td>
+                            <td>
+                                <a href="home.php?edit=<?php echo $row['PexpenseID']; ?>"
+                                    >Edit</a>
+                                <a href="process.php?delete=<?php echo $row['PexpenseID']; ?>"
+                                    >Delete</a>
+                            </td>
+                        </tr>
+                            
+                    <!--Ending the Loop-->
+                    <?php endwhile;?>
+                </table>
+            </div>
+
+        
+        <?php
+        //function to print fetched array
+        function pre_r($array){
+            echo '<pre>';
+            print_r($array);
+            echo '</pre>';
+        }
         ?>
+
+
+
+        <!--ADD Amount Form -->
+        <form action="process.php" method="POST">
+            <div class="container">
+                <input type="hidden" name="PexpenseID" value="<?php echo $PexpenseID ?>">
+                <label>Description</label>
+                <input type="text" name="descr" 
+                        value="<?php echo $descr; ?>" placeholder="Description" required>
+                <label>Amount</label>
+                <input type="number" name="amount" 
+                        value="<?php echo $amount; ?>" placeholder="Amount" required/>
+                <?php 
+                if ($update == true):
+                ?>
+                    <button type="submit" name="update">Update</button>
+                <?php else: ?>
+                    <button type="submit" name="add">ADD</button>
+                <?php endif; ?>
+            </div>
+        </form>
     </body>
 </html>
---
+++ project/process.php
@@ -0,0 +1,69 @@
+<?php
+
+
+include('config.php');
+include('login.php');
+$username=$_SESSION['username'];
+$user_pexpenses=$username.'_Pexpenses';
+$PexpenseID=0;
+$update=false;
+$descr = '';
+$amount = '';
+
+/// For Add
+if(isset($_POST['add'])){
+    $descr = $_POST['descr'];
+    $amount = $_POST['amount'];
+
+    // $con->query("INSERT INTO $user_pexpenses (descr,amount,ds,ts) VALUES($descr,$amount,getdate(),getdate())");
+    $addsql = "INSERT INTO $user_pexpenses (descr,amount,ds,ts) VALUES(?,?,?,?)";
+    $stmtadd = $con->prepare($addsql);     // ???????????????????????
+    $result = $stmtadd->execute([$descr,$amount,getdate(),gettimeofday()]); 
+    
+    $_SESSION['message'] = "Record Has Been Saved";
+    $_SESSION['msg_type'] = "Success";
+    header("Location: home.php?=Succesfully Added");
+}
+
+/// For Delete
+if(isset($_GET['delete'])){
+    $PexpenseID = $_GET['delete'];
+
+    $con->query("DELETE FROM $user_pexpenses where PexpenseID=$PexpenseID");
+
+    $_SESSION['message'] = "Record Has Been Deleted";
+    $_SESSION['msg_type'] = "Danger";
+    header("Location: home.php?=Succesfully Deleted");
+}
+
+//// To get the values in the form after clicking edit
+if(isset($_GET['edit'])){
+    
+    $PexpenseID = $_GET['edit'];
+    $result = $con->query("SELECT * FROM $user_pexpenses WHERE PexpenseID=$PexpenseID");
+    $update=true;
+    $row = $result->fetch_array();
+    $descr = $row['descr'];
+    $amount = $row['amount'];
+    // if (count($result)==1){
+    //     $row = $result->fetch_array();
+    //     $descr = $row['descr'];
+    //     $amount = $row['amount'];
+    // }
+
+}
+
+
+//// To update the value
+if (isset($_POST['update'])){
+    $PexpenseID = $_POST['PexpenseID'];
+    $descr = $_POST['descr'];
+    $amount = $_POST['amount'];
+
+    $result = $con->query("UPDATE $user_pexpenses SET descr='$descr',amount=$amount WHERE PexpenseID=$PexpenseID");
+    $_SESSION['message'] = "Record has been updated";
+    $_SESSION['msg_type'] = 'warning';
+    header("Location: home.php?=Successfully Updated");
+}
+?>
+
