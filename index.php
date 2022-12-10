f6f3bdd12e1aaa5d3d6a154f1be08940ec53ebf3
Mehedi Hasan Nabil <37480357+NOCKTOWL@users.noreply.github.com>
2022-12-11 01:50:58 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 
     <title>LOGIN</title>
 
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/index/index.css">
 
 </head>
 
@@ -15,7 +16,7 @@
 
      <form action="login.php" method="POST">
 
-        <h2>LOGIN</h2>
+        <h1>LOGIN</h1>
 
         <?php if (isset($_GET['error'])) { ?>
 
@@ -23,16 +24,38 @@
 
         <?php } ?>
 
-        <label>Username</label>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="form-group">
+                        <input class="form-input" type="text" name="username" required>
+                        <label class="form-label" for="username">Username</label>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="form-group">
+                        <input class="form-input" type="password" name="pass" required> 
+                        <label class="form-label" for="pass">Password</label>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <button class="button" type="submit">Login</button>
+                </div>
+                <div class="col-sm-6">
+                    <button class="button" type="submit"><a href="signup.php">Sign Up</a></button>
+                </div>
+                <div class="col-sm-12">
+                    <button class="button" type="submit"><a href="#">Forgot your password?</a></button>
+                </div>
+            </div>
+        </div>
+        
 
-        <input type="text" name="username" placeholder="username"><br>
-
-        <label>Password</label>
-
-        <input type="pass" name="pass" placeholder="Password"><br> 
-
-        <button type="submit">Login</button>
-        <button type="submit"><a href="signup.php">Sign Up</a></button>
+        
+        
+        
+        
+        
 
      </form>
 
