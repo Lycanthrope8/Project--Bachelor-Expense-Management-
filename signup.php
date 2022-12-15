140d94abb084079bd6aca2e4815813b9df69dcd4
Mehedi Hasan Nabil <37480357+NOCKTOWL@users.noreply.github.com>
2022-12-15 16:53:13 +0600

    DESIGN V1.2

--- signup.php
+++ signup.php
@@ -8,13 +8,10 @@ include('config.php');
     <head>
         <title>Registration</title>
 
-        <!-- <link rel="stylesheet" type="text/css" href="assets/css/style.css"> -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     </head>  
 <body>
-    <style>
-        background: red;
-    </style>
     <div>
         <?php
         if (isset($_POST['register'])){           // register came from input type submit
